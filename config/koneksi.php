2bd19a1fbeed88a5afdc39f3d9c07b76a7b3dddd
kent-fortin <Kentfh209@gmail.com>
2026-07-17 15:18:20 +0700

    Fix bug insert karyawan by adding auto-migration for ktp/kk

--- config/koneksi.php
+++ config/koneksi.php
@@ -31,6 +31,12 @@ $db = 'if0_42362934_db_payroll_star_samudera';
 $conn = mysqli_connect($host, $user, $pass, $db);
 if ($conn) {
     mysqli_set_charset($conn, 'utf8mb4');
+    
+    // Auto-migrate for missing columns
+    $qKaryawan = @mysqli_query($conn, "SHOW COLUMNS FROM karyawan LIKE 'no_ktp'");
+    if ($qKaryawan && mysqli_num_rows($qKaryawan) == 0) {
+        @mysqli_query($conn, "ALTER TABLE karyawan ADD no_ktp VARCHAR(20) NULL, ADD no_kk VARCHAR(20) NULL");
+    }
 } else {
     die(mysqli_connect_error());
 }
